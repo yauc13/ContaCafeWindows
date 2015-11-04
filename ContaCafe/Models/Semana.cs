08815f622bb29a25ccf71659d288a3bb5f4ba23f
Yeison <yauc1317@gmail.com>
2015-11-04 16:19:15 -0500

    creacion de clase Semana y SemanaParse, ademas se sube al repositorio

---
+++ ContaCafe/Models/Semana.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCafe.Models
+{
+   public class Semana
+    {
+        public string IdSemana { get; set; }
+        public string NombreSemana { get; set; }
+
+        public Semana()
+        {
+
+        }
+
+        public Semana(string NombreSemana)
+        {
+            this.NombreSemana = NombreSemana;
+        }
+
+
+    }
+}
