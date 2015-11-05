8a547b8a069529ab99bf71d52b85962ec0cf8053
Yeison <yauc1317@gmail.com>
2015-11-05 12:43:43 -0500

    creacion de Pagina menu de inicio

--- ContaCafe/App.xaml.cs
+++ ContaCafe/App.xaml.cs
@@ -78,7 +78,7 @@ namespace ContaCafe
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(AgregarInsumoPage), e.Arguments);
+                rootFrame.Navigate(typeof(ListaInsumoPage), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
